315a9f0ff4bc9d9bfdb05249b84438439dc70add
Susanta <inbox.susanta@gmail.com>
2018-09-22 22:10:58 +0530

    Looping with v-for

--- Vue-Basics/app.js
+++ Vue-Basics/app.js
@@ -2,16 +2,13 @@ new Vue({
     el: '#app',
     data: {
       title: 'Becoming a Vue ninja',
-      showName: false,
-      showAge: true
-
+      items: ['Mashroom', 'Green Shells', 'Red Shells', 'Banana', 'star'],
+      ninjas: [
+        {name: 'Crystal', age: 25, belt: 'black'},
+        {name: 'Ryu', age: 30, belt: 'brown'},
+        {name: 'ken', age: 35, belt: 'orange'}
+      ]
     },
     methods: {
-      toggleName() {
-        this.showName = !this.showName
-      },
-      toggleAge() {
-        this.showAge = !this.showAge
-      }
     }
 })
