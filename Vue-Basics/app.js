e900b136ff717dc0d270cd55262a2faa33561cb3
Susanta <inbox.susanta@gmail.com>
2018-09-22 21:57:32 +0530

    COnditional output with v-if

--- Vue-Basics/app.js
+++ Vue-Basics/app.js
@@ -2,11 +2,16 @@ new Vue({
     el: '#app',
     data: {
       title: 'Becoming a Vue ninja',
-      name: 'Ryu'
+      showName: false,
+      showAge: true
+
     },
     methods: {
-      logMessage() {
-        console.log('Hello')
+      toggleName() {
+        this.showName = !this.showName
+      },
+      toggleAge() {
+        this.showAge = !this.showAge
       }
     }
 })
